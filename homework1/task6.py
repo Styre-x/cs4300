cd7e419776f91511e37f58efa016c0fb0f1743c2
Styre-x <robotgarland@gmail.com>
2025-02-02 22:48:19 -0700

    add metaprogramming to count specific words

--- homework1/task6.py
+++ homework1/task6.py
@@ -1,13 +1,39 @@
-with open("task6_read_me.txt") as file:
-    read = file.read()
-    file.close()
+# based off what is said by google:
+# something that allows for programmatic generation of code.
+# I hope this works.
+# I have never "metaprgrammed" before
 
-#TODO:
-# Implement metaprogramming techniques to dynamically generate function names
-# for your pytest test cases based on the filenames of the text files. Include pytest test cases that verify
-# the word count for each text file.
-# what is metaprogramming??????
-# ???
+class WordCounter:
+    def __init__(self, text):
+        self.text = text.lower()
+        self.words = self.text.split()
+    
+    def count_word(self, word):
+        return self.words.count(word.lower())
 
-def test_wordcount():
-    assert len(read.split()) == 127
+    @classmethod
+    def generate_counter_methods(cls, *words):
+        """Dynamically generates methods for counting specific words."""
+        for word in words:
+            method_name = f'count_{word}'
+            setattr(cls, method_name, cls._create_counter_method(word))
+    
+    @staticmethod
+    def _create_counter_method(word):
+        def counter(self):
+            return self.count_word(word)
+        return counter
+
+# Test cases using pytest
+def test_word_counter():
+    with open("task6_read_me.txt") as file:
+        read = file.read()
+        file.close()
+    text = read
+    counter = WordCounter(text)
+    
+    WordCounter.generate_counter_methods("lorem", "nam", "tristique")
+    
+    assert counter.count_lorem() == 3
+    assert counter.count_nam() == 1
+    assert counter.count_tristique() == 1
